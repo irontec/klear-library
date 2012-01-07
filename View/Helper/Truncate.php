9524359322f86fa6d53927ea5a18d97c67977f02
Alayn Gortazar <zutoin@gmail.com>
2012-01-07 23:51:53 +0000

    bugfixes y test para Truncate
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@11 1c59303b-82d5-4f45-9987-d8202ac5da96

--- View/Helper/Truncate.php
+++ View/Helper/Truncate.php
@@ -6,26 +6,26 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
 
 
     /**
-     * Recibe $texto y devuelve $texto truncado a $max_length caracteres.
+     * Recibe $text y devuelve $text truncado a $maxLength caracteres.
      * Si es html ($isHTML), se encargará de cerrar todos los tags abiertos (y no contarlos para max_length)
      * Siempre que se trunque el texto, se añadirá $extra al final del string devuelto
      *
      * Uso (Desde la vista)
      * $this->truncate($registro,20,'<a href="#more">[más]</a>',true);
      *
-     * @param string $texto
-     * @param int $max_length
+     * @param string $text
+     * @param int $maxLength
      * @param string $extra
      * @param bool $isHTML
      * @return string
      */
-    public function truncate($texto, $max_length, $extra = '...', $isHTML = true){
+    public function truncate($text, $maxLength = 150, $extra = '...', $isHTML = true){
         $i = 0;
         $tags = array();
         if($isHTML){
-            preg_match_all('/<[^>]+>([^<]*)/', $string, $res, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+            preg_match_all('/<[^>]+>([^<]*)/', $text, $res, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
             foreach($res as $o){
-                if($o[0][1] - $i >= $length)
+                if($o[0][1] - $i >= $maxLength)
                     break;
                 $t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
                 if($t[0] != '/')
@@ -35,6 +35,6 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
                 $i += $o[1][1] - $o[0][1];
             }
         }
-        return substr($string, 0, $length = min(strlen($string),  $length + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . (strlen($string) > $length ? $extra : '');
+        return substr($text, 0, $maxLength = min(strlen($text),  $maxLength + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . (strlen($text) > $maxLength ? $extra : '');
     }
 }
