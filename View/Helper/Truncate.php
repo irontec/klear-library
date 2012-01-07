a8f6e99acfa64487315ca153df97835004f2fb79
Alayn Gortazar <zutoin@gmail.com>
2012-01-07 23:59:43 +0000

    Arreglando estilos
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@12 1c59303b-82d5-4f45-9987-d8202ac5da96

--- View/Helper/Truncate.php
+++ View/Helper/Truncate.php
@@ -7,7 +7,7 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
 
     /**
      * Recibe $text y devuelve $text truncado a $maxLength caracteres.
-     * Si es html ($isHTML), se encargará de cerrar todos los tags abiertos (y no contarlos para max_length)
+     * Si es html ($isHtml), se encargará de cerrar todos los tags abiertos (y no contarlos para max_length)
      * Siempre que se trunque el texto, se añadirá $extra al final del string devuelto
      *
      * Uso (Desde la vista)
@@ -16,25 +16,36 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
      * @param string $text
      * @param int $maxLength
      * @param string $extra
-     * @param bool $isHTML
+     * @param bool $isHtml
      * @return string
      */
-    public function truncate($text, $maxLength = 150, $extra = '...', $isHTML = true){
+    public function truncate($text, $maxLength = 150, $extra = '...', $isHtml = true)
+    {
         $i = 0;
         $tags = array();
-        if($isHTML){
-            preg_match_all('/<[^>]+>([^<]*)/', $text, $res, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
-            foreach($res as $o){
-                if($o[0][1] - $i >= $maxLength)
+        if ($isHtml) {
+            preg_match_all(
+                    '/<[^>]+>([^<]*)/',
+                    $text,
+                    $result,
+                    PREG_OFFSET_CAPTURE | PREG_SET_ORDER
+            );
+
+            foreach ($result as $object) {
+                if ($object[0][1] - $i >= $maxLength) {
                     break;
-                $t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
-                if($t[0] != '/')
-                    $tags[] = $t;
-                elseif(end($tags) == substr($t, 1))
+                }
+                $tag = substr(strtok($object[0][0], " \t\n\r\0\x0B>"), 1);
+                if($tag[0] != '/') {
+                    $tags[] = $tag;
+                } elseif(end($tags) == substr($tag, 1)) {
                     array_pop($tags);
-                $i += $o[1][1] - $o[0][1];
+                }
+                $i += $object[1][1] - $object[0][1];
             }
         }
-        return substr($text, 0, $maxLength = min(strlen($text),  $maxLength + $i)) . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . (strlen($text) > $maxLength ? $extra : '');
+        return substr($text, 0, $maxLength = min(strlen($text),  $maxLength + $i))
+                . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '')
+                . (strlen($text) > $maxLength ? $extra : '');
     }
 }
