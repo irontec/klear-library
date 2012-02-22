4b0802b63286955f8484e40de94791b2d18935b1
Alayn Gortazar <zutoin@gmail.com>
2012-02-22 12:45:13 +0000

    Extensión del ContextSwitch que se encarga de convertir en arrays los objetos pasados a un context json
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@50 1c59303b-82d5-4f45-9987-d8202ac5da96

---
+++ Controller/Action/Helper/IronContextSwitch.php
@@ -0,0 +1,49 @@
+<?php
+require_once('Zend/Controller/Action/Helper/Abstract.php');
+
+/**
+ * @author Alayn Gortazar <alayn+karma@irontec.com>
+ *
+ * Helper que extiende el ContextSwitch y se encarga de convertir en arrays todos los objetos de la vista
+ * que contengan el método "toArray" siempre y cuando se encuentre bajo el contexto "Json"
+ *
+ */
+class Iron_Controller_Action_Helper_IronContextSwitch extends Zend_Controller_Action_Helper_ContextSwitch
+{
+    /**
+     * JSON post processing
+     *
+     * JSON serialize view variables to response body
+     *
+     * @return void
+     */
+    public function postJsonContext()
+    {
+        if (!$this->getAutoJsonSerialization()) {
+            return;
+        }
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
+        $view = $viewRenderer->view;
+        if ($view instanceof Zend_View_Interface) {
+            /**
+             * @see Zend_Json
+             */
+            if(method_exists($view, 'getVars')) {
+
+                $viewVars = $view->getVars();
+                foreach ($viewVars as $name => $param) {
+                    if (is_object($param) && method_exists($param, 'toArray')) {
+                        $viewVars[$name] = $param->toArray();
+                    }
+                }
+
+                require_once 'Zend/Json.php';
+                $vars = Zend_Json::encode($viewVars);
+                $this->getResponse()->setBody($vars);
+            } else {
+                require_once 'Zend/Controller/Action/Exception.php';
+                throw new Zend_Controller_Action_Exception('View does not implement the getVars() method needed to encode the view into JSON');
+            }
+        }
+    }
+}
