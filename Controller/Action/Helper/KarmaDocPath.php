499126b702ab7059d6c2158b8c923fec7ad47bb6
Alayn Gortazar <zutoin@gmail.com>
2012-03-01 13:22:52 +0000

    KarmaDocPath permite devolver rutas no existentes si le pasamos un tercer parámetro
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@53 1c59303b-82d5-4f45-9987-d8202ac5da96

--- Controller/Action/Helper/KarmaDocPath.php
+++ Controller/Action/Helper/KarmaDocPath.php
@@ -14,9 +14,10 @@ class Iron_Controller_Action_Helper_KarmaDocPath extends Zend_Controller_Action_
      * @param  string $docsRoot Directorio donde se encuentras los documentos
      *          (p.e. APPLICATION_PATH . '/data/documentos.plt/'
      * @param  $id Id del documento que se desea obtener
-     * @return string|null Ruta real en la que se encuentra el fichero en el disco. Si no existe devuelve null
+     * @param  $checkExists Comprobar si el fichero existe o no.
+     * @return string|null Ruta real en la que se encuentra el fichero en el disco. Si no existe devuelve null (dependiendo del parámetro $checkExists)
      */
-    public function getDocumentPath($docsRoot, $id)
+    public function getDocumentPath($docsRoot, $id, $checkExists = true)
     {
         $aId = str_split((string)$id);
         array_pop($aId);
@@ -25,8 +26,13 @@ class Iron_Controller_Action_Helper_KarmaDocPath extends Zend_Controller_Action_
         }
         $aId[] = $id;
         $filePath = $docsRoot . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $aId);
-        if (is_file($filePath)) {
-            return realpath($filePath);
+
+        if ($checkExists) {
+            if (is_file($filePath)) {
+                return realpath($filePath);
+            }
+        } else {
+            return $filePath;
         }
         return null;
     }
@@ -37,10 +43,10 @@ class Iron_Controller_Action_Helper_KarmaDocPath extends Zend_Controller_Action_
      * @param  string $docsRoot Directorio donde se encuentras los documentos
      *          (p.e. APPLICATION_PATH . '/data/documentos.plt/'
      * @param  $id Id del documento que se desea obtener
-     * @return string|null Ruta real en la que se encuentra el fichero en el disco. Si no existe devuelve null
+     * @return string|null Ruta real en la que se encuentra el fichero en el disco. Si no existe devuelve null (dependiendo del parámetro $checkExists)
      */
-    public function direct($docsRoot, $id)
+    public function direct($docsRoot, $id, $checkExists = true)
     {
-        return $this->getDocumentPath($docsRoot, $id);
+        return $this->getDocumentPath($docsRoot, $id, $checkExists);
     }
 }
