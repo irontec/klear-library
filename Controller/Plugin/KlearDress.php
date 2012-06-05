17484f5396c1496543d3264b51fe1c977a931a25
Javier Infante <jabi+github@irontec.com>
2012-06-05 19:47:11 +0000

    Disfraz para que klear sea servido desde / sin ser detectado ;)
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@76 1c59303b-82d5-4f45-9987-d8202ac5da96

---
+++ Controller/Plugin/KlearDress.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin que sobre-escribe la petición a un / para que ésta vaya directamente a klear
+ * (sin mostra /klear en la URL; aunque si en el resto de peticiones por debajo)
+ * 
+ */
+class Iron_Controller_Plugin_KlearDress extends Zend_Controller_Plugin_Abstract
+{
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
+
+        if (    $request->getModuleName() == 'default' && 
+                $request->getControllerName() == 'index' ) {
+        
+
+                $request->setModuleName('klear')
+                    ->setControllerName('index')
+                    ->setDispatched(false);
+        }
+        
+    }
+}
