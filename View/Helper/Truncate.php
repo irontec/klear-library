4a1d8970552acc344b4d3b59dcd81e72171e9a51
Alayn Gortazar <zutoin@gmail.com>
2012-10-16 10:16:44 +0000

    Added option to insert the $extra text inside the html tags.
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@87 1c59303b-82d5-4f45-9987-d8202ac5da96

--- View/Helper/Truncate.php
+++ View/Helper/Truncate.php
@@ -3,7 +3,8 @@ require_once('Zend/View/Helper/Abstract.php');
 
 class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
 {
-
+    protected $_tags = array();
+    protected $_extraInline = false;
 
     /**
      * Recibe $text y devuelve $text truncado a $maxLength caracteres.
@@ -19,10 +20,13 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
      * @param bool $isHtml
      * @return string
      */
-    public function truncate($text, $maxLength = 150, $extra = '...', $isHtml = true)
+    public function truncate($text, $maxLength = 150, $extra = '...', $isHtml = true, $extraInline = false)
     {
+        $this->_extraInline = $extraInline;
+
         $i = 0;
-        $tags = array();
+        $this->_tags = array();
+
         if ($isHtml) {
             preg_match_all(
                 '/<[^>]+>([^<]*)/',
@@ -37,15 +41,44 @@ class Iron_View_Helper_Truncate extends Zend_View_Helper_Abstract
                 }
                 $tag = substr(strtok($object[0][0], " \t\n\r\0\x0B>"), 1);
                 if ($tag[0] != '/') {
-                    $tags[] = $tag;
-                } else if (end($tags) == substr($tag, 1)) {
-                    array_pop($tags);
+                    $this->_tags[] = $tag;
+                } else if (end($this->_tags) == substr($tag, 1)) {
+                    array_pop($this->_tags);
                 }
                 $i += $object[1][1] - $object[0][1];
             }
         }
-        return substr($text, 0, $maxLength = min(strlen($text), $maxLength + $i))
-                . (count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '')
-                . (strlen($text) > $maxLength ? $extra : '');
+
+        $shortenedText = substr($text, 0, $maxLength = min(strlen($text), $maxLength + $i));
+        $shortenedText .= $this->_getExtraText($text, $maxLength, $extra);
+        return $shortenedText;
+    }
+
+    protected function _getExtraText($text, $maxLength, $extra)
+    {
+        if ($this->_extraInline) {
+            return $this->_getExtra($text, $maxLength, $extra)
+                 . $this->_getClosingTags();
+        } else {
+            return $this->_getClosingTags()
+                 . $this->_getExtra($text, $maxLength, $extra);
+        }
+
+    }
+
+    protected function _getClosingTags()
+    {
+        if (count($this->_tags)) {
+            $tags = array_reverse($this->_tags);
+            return '</' . implode('></', $tags) . '>';
+        }
+        return '';
+    }
+
+    protected function _getExtra($text, $maxLength, $extra) {
+        if (strlen($text) > $maxLength) {
+            return $extra;
+        }
+        return '';
     }
 }
