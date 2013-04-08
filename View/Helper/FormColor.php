41deaad9307b45e4d86f43416428309e4170bd53
Mikel Madariaga <mikel@irontec.com>
2013-04-08 14:09:37 +0000

    Elementos tipo texto html5: Iron_Form_Element_Html5_[Color|Email|Number|Range|Url] y sus view helper
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@112 1c59303b-82d5-4f45-9987-d8202ac5da96

---
+++ View/Helper/FormColor.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'Iron/View/Helper/FormHtml5.php';
+
+/**
+ * Base Helper to generate a html5 "email" text element
+ *
+ * @category   Iron
+ * @package    Iron_View
+ * @subpackage Helper
+ */
+class Iron_View_Helper_FormColor extends Iron_View_Helper_FormHtml5
+{
+    /**
+     * @var string
+     */
+    protected $_type = 'color';
+
+    /**
+     * Generates a 'text' element.
+     *
+     * @access public
+     *
+     * @param string|array $name If a string, the element name.  If an
+     * array, all other parameters are ignored, and the array elements
+     * are used in place of added parameters.
+     *
+     * @param mixed $value The element value.
+     *
+     * @param array $attribs Attributes for the element tag.
+     *
+     * @return string The element XHTML.
+     */
+    public function formColor($name, $value = null, $attribs = null)
+    {
+        return $this->_formHtml5Element($name, $value, $attribs);
+    }
+}
---
+++ View/Helper/FormHtml5.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Abstract class for extension
+ */
+require_once 'Zend/View/Helper/FormElement.php';
+
+
+/**
+ * Base Helper to generate a "html5" text element (url, email...)
+ *
+ * @category   Iron
+ * @package    Iron_View
+ * @subpackage Helper
+ */
+Abstract class Iron_View_Helper_FormHtml5 extends Zend_View_Helper_FormElement
+{
+    /**
+     * @var string
+     */
+    protected $_type;
+
+    /**
+     * Generates a 'text' element.
+     *
+     * @access public
+     *
+     * @param string|array $name If a string, the element name.  If an
+     * array, all other parameters are ignored, and the array elements
+     * are used in place of added parameters.
+     *
+     * @param mixed $value The element value.
+     *
+     * @param array $attribs Attributes for the element tag.
+     *
+     * @return string The element XHTML.
+     */
+    protected function _formHtml5Element($name, $value = null, $attribs = null)
+    {
+        $info = $this->_getInfo($name, $value, $attribs);
+        extract($info); // name, value, attribs, options, listsep, disable
+
+        // build the element
+        $disabled = '';
+        if ($disable) {
+            // disabled
+            $disabled = ' disabled="disabled"';
+        }
+
+        // XHTML or HTML end tag?
+        $endTag = ' />';
+        if (($this->view instanceof Zend_View_Abstract) && !$this->view->doctype()->isXhtml()) {
+            $endTag= '>';
+        }
+
+        $xhtml = '<input type="'. $this->_type .'"'
+                . ' name="' . $this->view->escape($name) . '"'
+                . ' id="' . $this->view->escape($id) . '"'
+                . ' value="' . $this->view->escape($value) . '"'
+                . $disabled
+                . $this->_htmlAttribs($attribs)
+                . $endTag;
+
+        return $xhtml;
+    }
+}
