1853c2d2dcbb16b6d0b27e447f0950d68dbf7973
Mikel Madariaga <mikel@irontec.com>
2013-04-24 09:10:59 +0000

    Trim del valor en Slug filter
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@120 1c59303b-82d5-4f45-9987-d8202ac5da96

--- Filter/Slug.php
+++ Filter/Slug.php
@@ -4,7 +4,7 @@ class Iron_Filter_Slug implements Zend_Filter_Interface
     public function filter($value)
     {
         $asciiFilter = new Iron_Filter_Ascii();
-        $asciiValue = $asciiFilter->filter($value);
+        $asciiValue = $asciiFilter->filter(trim($value));
 
         $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $asciiValue);
         $slug = strtolower(trim($slug, '-'));
