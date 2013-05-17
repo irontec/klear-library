7c91be3f04864c86c19af015c4f2659175f16d96
Javier Infante <jabi+github@irontec.com>
2013-05-17 10:24:37 +0000

    Worker y Manager para Iron. TODO: Documentar
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@126 1c59303b-82d5-4f45-9987-d8202ac5da96

---
+++ Gearman/Manager.php
@@ -0,0 +1,93 @@
+<?php
+class Iron_Gearman_Manager
+{
+    private static $_gearmanServers = null;
+    private static $_stdLogger = null;
+    private static $_options = null;
+
+
+    public static function setOptions($options)
+    {
+        self::$_options = $options;
+
+    }
+
+    /**
+     * Retrieves the current Gearman Servers
+     *
+     * @return array
+     */
+    public static function getServers()
+    {
+        if (self::$_gearmanServers === null) {
+
+            self::$_gearmanServers = implode(",", self::$_options['servers']);
+
+        }
+
+        return self::$_gearmanServers;
+    }
+
+    /**
+     * Creates a GearmanClient instance and sets the job servers
+     *
+     * @return GearmanClient
+     */
+    public static function getClient()
+    {
+        $gmclient= new \GearmanClient();
+        $servers = self::getServers();
+        $gmclient->addServers($servers);
+
+        return $gmclient;
+    }
+
+    /**
+     * Creates a GearmanWorker instance
+     *
+     * @return GearmanWorker
+     */
+    public static function getWorker()
+    {
+        $worker = new GearmanWorker();
+        $servers = self::getServers();
+        
+        $worker->addServers($servers);
+
+        return $worker;
+    }
+
+    /**
+     * Given a worker name, it checks if it can be loaded. If it's possible,
+     * it creates and returns a new instance.
+     *
+     * @param string $workerName
+     * @param string $logFile
+     * @return Model_Gearman_Worker
+     */
+    public static function runWorker($workerName, $logFile = null)
+    {
+        $workerName .= 'Worker';
+        $workerFile = APPLICATION_PATH . '/workers/' . $workerName . '.php';
+
+        if (!file_exists($workerFile)) {
+            throw new InvalidArgumentException(
+                    "El Worker no existe: {$workerFile}"
+            );
+        }
+
+        require $workerFile;
+
+        if (!class_exists($workerName)) {
+            throw new InvalidArgumentException(
+                    "La clase {$workerName} no existe en el archivo: {$workerFile}"
+            );
+        }
+
+        return new $workerName($logFile);
+    }
+
+
+
+
+}
---
+++ Gearman/Worker.php
@@ -0,0 +1,171 @@
+<?php
+
+class Iron_Gearman_Worker
+{
+    /**
+     * Register Function
+     * @var string
+     */
+    protected $_registerFunction;
+
+    /**
+     * Gearman Timeout
+     * @var int
+     */
+    protected $_timeout = 60000;
+
+    /**
+     * Alloted Memory Limit in MB
+     * @var int
+     */
+    protected $_memory = 1024;
+
+    /**
+     * Error Message
+     * @var string
+     */
+    protected $_error = null;
+
+    /**
+     * Gearman Worker
+     * @var GearmanWorker
+     */
+    protected $_worker;
+
+    /**
+     * Memory limit activation
+     * @var bool
+     */
+    protected $_enableMemoryLimit = false;
+
+    protected $_logFile = null;
+
+    /**
+     * Constructor
+     * Checks for the required gearman extension,
+     * fetches the bootstrap and loads in the gearman worker
+     */
+    public function __construct($logFile = null)
+    {
+        $this->initRegisterFunctions();
+
+        // Check extension
+        if (!extension_loaded('gearman')) {
+            throw new RuntimeException('The PECL::gearman extension is required.');
+        }
+
+        if (!empty($logFile)) {
+            $this->_logFile = $logFile;
+        }
+
+        // Creates a new Gearman Worker and set its servers
+        $this->_worker = EKT_Gearman_Manager::getWorker();
+
+        // Checks the registerFunction
+        if (empty($this->_registerFunction)) {
+            throw new InvalidArgumentException(get_class($this)
+                    . ' must implement a registerFunction');
+        }
+
+        // allow for a small memory gap:
+        if ($this->_enableMemoryLimit) {
+            $memoryLimit = ($this->_memory + 128) * 1024 * 1024;
+            ini_set('memory_limit', $memoryLimit);
+        }
+
+        // Registers
+        if (is_array($this->_registerFunction)) {
+            foreach ($this->_registerFunction as $alias => $func) {
+                $this->_worker->addFunction($alias, array($this, $func));
+            }
+        } else {
+            $this->_worker->addFunction($this->_registerFunction, array($this, 'work'));
+        }
+
+        $this->_worker->setTimeout($this->_timeout);
+
+        $this->init();
+
+        if (!empty($this->_logFile)) {
+            //Model_Gearman_Manager::getLogger($this->_logFile)->log('[Worker initialized]', Zend_Log::INFO);
+        }
+
+        while ($this->_worker->work() ||
+                $this->_worker->returnCode() == GEARMAN_TIMEOUT) {
+
+            // if a timeout ocurrs
+            if ($this->_worker->returnCode() == GEARMAN_TIMEOUT) {
+                $this->timeout();
+                continue;
+            }
+
+            // if an error ocurrs
+            if ($this->_worker->returnCode() != GEARMAN_SUCCESS) {
+                $this->setError($this->_worker->returnCode() . ': ' . $this->_worker->getErrno() . ': ' . $this->_worker->error());
+                break;
+            }
+        }
+
+        $this->shutdown();
+    }
+
+    /**
+     * Initialization of Register functions
+     *
+     * @return void
+     */
+    protected function initRegisterFunctions()
+    {
+
+    }
+
+    /**
+     * Initialization
+     *
+     * @return void
+     */
+    protected function init()
+    {
+
+    }
+
+    /**
+     * Handle Timeout
+     *
+     * @return void
+     */
+    protected function timeout()
+    {
+    }
+
+    /**
+     * Handle Shutdown
+     *
+     * @return void
+     */
+    protected function shutdown()
+    {
+
+    }
+
+    /**
+     * Set Error Message
+     *
+     * @param string $error
+     * @return void
+     */
+    public function setError($error)
+    {
+        $this->_error = $error;
+    }
+
+    /**
+     * Get Error Message
+     *
+     * @return string|null
+     */
+    public function getError()
+    {
+        return $this->_error;
+    }
+}
