fe126e3635db8ef4a8f29c327bbdd4d8008ad30f
Alayn Gortazar <zutoin@gmail.com>
2013-05-27 08:06:31 +0000

    Simplificando el simpleLink
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@129 1c59303b-82d5-4f45-9987-d8202ac5da96

--- View/Helper/SimpleLink.php
+++ View/Helper/SimpleLink.php
@@ -5,7 +5,7 @@ class Iron_View_Helper_SimpleLink extends Zend_View_Helper_Abstract
      * Returns a link generated with suplied data
      * @param string $url Url of the link
      * @param string $text Text to show on the link. If null, the $url will be shown
-     * @param array $attribs Attribs for the anchor tag. (class, title, etc...)
+     * @param array $attribs Attribs for the anchor tag. (class, title, etc...). It may also admit some
      * @return string
      */
     public function simpleLink($url, $text = null, $attribs = array())
@@ -17,28 +17,16 @@ class Iron_View_Helper_SimpleLink extends Zend_View_Helper_Abstract
             return '';
         }
 
-        if (!$finalText) {
-            if (isset($attribs['domainOnly']) && $attribs['domainOnly']) {
-                $uri = Zend_Uri_Http::fromString($finalUrl);
-                $showText = $uri->getHost();
-            } else {
-                $showText = $finalUrl;
-            }
-        } else{
-            $showText = $finalText;
-        }
-
-        if (isset($attribs['hash'])) {
-            $finalUrl .= '#' . $attribs['hash'];
-            unset ($attribs['hash']);
-        }
-
         $attrString = ' ';
         foreach ($attribs as $key => $value) {
             $attrString .= $key . '="' . $value . '"';
         }
 
-        return '<a href="' . $finalUrl .'" ' . $attrString . '>' . $this->view->escape($showText) . '</a>';
+        if (!$finalText) {
+            $finalText = $finalUrl;
+        }
+
+        return '<a href="' . $finalUrl .'" ' . $attrString . '>' . $this->view->escape($finalText) . '</a>';
 
     }
 }
