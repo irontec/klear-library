17a30c553fe253147f1822fa65a0ed54629e1f9c
Alayn Gortazar <zutoin@gmail.com>
2013-06-07 10:08:49 +0000

    Permitimos no escapar el texto al crear el enlace
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@133 1c59303b-82d5-4f45-9987-d8202ac5da96

--- View/Helper/SimpleLink.php
+++ View/Helper/SimpleLink.php
@@ -6,9 +6,10 @@ class Iron_View_Helper_SimpleLink extends Zend_View_Helper_Abstract
      * @param string $url Url of the link
      * @param string $text Text to show on the link. If null, the $url will be shown
      * @param array $attribs Attribs for the anchor tag. (class, title, etc...). It may also admit some
+     * @param string $escapeText Bool Escape text or not. By default text is escaped to avoid XSS
      * @return string
      */
-    public function simpleLink($url, $text = null, $attribs = array())
+    public function simpleLink($url, $text = null, $attribs = array(), $escapeText = true)
     {
         $finalUrl = trim($url);
         $finalText = trim($text);
@@ -17,16 +18,20 @@ class Iron_View_Helper_SimpleLink extends Zend_View_Helper_Abstract
             return '';
         }
 
+        if (!$finalText) {
+            $finalText = $finalUrl;
+        }
+
+        if ($escapeText) {
+            $finalText = $this->view->escape($finalText);
+        }
+
         $attrString = ' ';
         foreach ($attribs as $key => $value) {
             $attrString .= $key . '="' . $value . '"';
         }
 
-        if (!$finalText) {
-            $finalText = $finalUrl;
-        }
-
-        return '<a href="' . $finalUrl .'" ' . $attrString . '>' . $this->view->escape($finalText) . '</a>';
+        return '<a href="' . $finalUrl .'" ' . $attrString . '>' . $finalText . '</a>';
 
     }
 }
