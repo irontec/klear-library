8c3d850128bbdd85fae6ee86669b24c6c2d46d92
Mikel Madariaga <mikel@irontec.com>
2013-09-05 17:00:44 +0000

    #33525 Fixer de rutas a imágenes internas por cambios de baseUrl
    git-svn-id: http://dev.irontec.com/svn/IronZend/trunk/library/Iron@150 1c59303b-82d5-4f45-9987-d8202ac5da96

---
+++ Model/PathFixer.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author Mikel Madariaga
+ *
+ * Comprueba las rutas de imágenes internas, reemplazando las rutas en caso de discordancia de baseUrl
+ *
+ */
+class Iron_Model_PathFixer
+{
+    protected $_originalHtml;
+
+    public function fix($html)
+    {
+        if (empty($html)) {
+            return $html;
+        }
+
+        $dom = new \DomDocument;
+        $dom->loadHTML(utf8_decode($html));
+        $images = $dom->getElementsByTagName('img');
+
+        $view = \Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('view');
+        $baseUrl = $view->baseUrl("/");
+
+        foreach ($images as $image) {
+            if ($image->hasAttribute('data-uri')) {
+                $uri = $image->getAttribute('data-uri');
+                $src = $image->getAttribute('src');
+
+                if ($src !== $baseUrl . $uri) {
+                    $image->setAttribute('src', $baseUrl . $uri);
+                }
+            }
+        }
+
+        $body = $dom->getElementsByTagName('body')->item(0);
+
+        if (!$body) {
+            return $html;
+        }
+
+        $wrapperChildList = $dom->firstChild->childNodes;
+        $fixedContent = '';
+        foreach ($body->childNodes as $node) {
+            $fixedContent .= $dom->saveHTML($node);
+        }
+
+        return $fixedContent;
+    }
+}
