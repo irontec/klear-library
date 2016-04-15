22c56fc9f654481e2dd9859be0ca8923f4df4326
Javier Infante <jabi@irontec.com>
2016-04-15 11:02:31 +0200

    quick FIX: comprobamos que la request tiene rawBody

--- Controller/Plugin/AutoParseRequestBody.php
+++ Controller/Plugin/AutoParseRequestBody.php
@@ -2,21 +2,23 @@
 
 /**
  * Plugin que comprueba el body de la peticición y completa la request con los campos encontrados
- 
+
  */
 class Iron_Controller_Plugin_AutoParseRequestBody extends Zend_Controller_Plugin_Abstract
 {
     public function routeShutdown(Zend_Controller_Request_Abstract $request) {
 
         //TODO: Investigar porque en fpm rquest no viene con el rawBody en content, y si ocurre en mpm
-        $requestBody = $request->getRawBody(); 
-        if (!empty($requestBody)) {
-        
-            //TODO: Comprobar Content-type del request para soportar JSON, XML, etc...
-            $requestBody = json_decode($requestBody, true);
-            if (!is_null($requestBody)) { 
-                foreach($requestBody as $param => $value) {
-                    $request->setParam($param, $value);
+        if (method_exists($request,'getRawBody')) {
+            $requestBody = $request->getRawBody();
+            if (!empty($requestBody)) {
+
+                //TODO: Comprobar Content-type del request para soportar JSON, XML, etc...
+                $requestBody = json_decode($requestBody, true);
+                if (!is_null($requestBody)) {
+                    foreach($requestBody as $param => $value) {
+                        $request->setParam($param, $value);
+                    }
                 }
             }
         }
