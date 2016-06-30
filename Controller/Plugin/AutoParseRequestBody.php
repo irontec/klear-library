8b8a9f6196cd7b2a19f0bc60ab67afb3a5c26cb9
ddniel16 <ddniel16@gmail.com>
2016-06-30 17:48:04 +0200

    No hacer nada si existe el parametro 'qqfile'

--- Controller/Plugin/AutoParseRequestBody.php
+++ Controller/Plugin/AutoParseRequestBody.php
@@ -2,25 +2,29 @@
 
 /**
  * Plugin que comprueba el body de la peticición y completa la request con los campos encontrados
-
  */
 class Iron_Controller_Plugin_AutoParseRequestBody extends Zend_Controller_Plugin_Abstract
 {
-    public function routeShutdown(Zend_Controller_Request_Abstract $request) {
+
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
 
         //TODO: Investigar porque en fpm rquest no viene con el rawBody en content, y si ocurre en mpm
-        if (method_exists($request,'getRawBody')) {
+        //TODO: Si existe el parametro 'qqfile' es que viene un fichero de klear en el body
+        if (method_exists($request, 'getRawBody') && !isset($_GET['qqfile'])) {
             $requestBody = $request->getRawBody();
             if (!empty($requestBody)) {
 
                 //TODO: Comprobar Content-type del request para soportar JSON, XML, etc...
                 $requestBody = json_decode($requestBody, true);
                 if (!is_null($requestBody)) {
-                    foreach($requestBody as $param => $value) {
+                    foreach ($requestBody as $param => $value) {
                         $request->setParam($param, $value);
                     }
                 }
             }
         }
+
     }
+
 }
